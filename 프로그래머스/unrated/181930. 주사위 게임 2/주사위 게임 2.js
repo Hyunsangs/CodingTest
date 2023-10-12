0fb426a22bd43a81e805ea8d5a6914a4b0e60dfa
Hyunsangs <122857450+Hyunsangs@users.noreply.github.com>
2023-10-12 16:46:51 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,18 @@
+function solution(a, b, c) {
+    let answer = 0;
+    
+    if ( a !== b && a !== c && b !== c) {
+        answer = a + b + c ;
+    }
+    
+    if ( (a === b && b !== c) || ( a === c &&  c !== b) || ( b === c && c !== a)){
+        answer = (a + b + c) * (a ** 2 + b ** 2 + c ** 2);
+    }
+    
+    if ( a === b && b === c) {
+        answer = ( a + b + c ) * ( (a ** 2 + b ** 2 + c ** 2) ) * (a ** 3 + b ** 3 + c ** 3);
+    }
+    
+    
+    return answer;
+}
