32c611956a1eba6260c1552202979aca5dfe2664
Hyunsangs <122857450+Hyunsangs@users.noreply.github.com>
2023-10-23 11:47:03 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.27 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -8,3 +8,11 @@ function solution(arr, queries) {
     }
     return arr;
 }
+/*
+function solution(arr, queries) {
+    queries.forEach( ([a,b]) => {
+        [arr[a],arr[b]] = [arr[b],arr[a]];
+    })
+    return arr;
+}
+*/
