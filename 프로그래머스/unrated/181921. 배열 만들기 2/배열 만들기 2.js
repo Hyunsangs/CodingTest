7f18aa736883b5f4f800267cb3df65eabe8d8b05
Hyunsangs <122857450+Hyunsangs@users.noreply.github.com>
2023-10-25 13:42:49 +0900

    [level unrated] Title: 배열 만들기 2, Time: 11.73 ms, Memory: 39.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -0,0 +1,11 @@
+function solution(l, r) {
+   const result = []
+   for (let i = l ; i <= r ; i ++) {
+        const charI = String(i)
+        if(![...charI].every(a => a === '5' || a === '0')) continue
+       result.push(i)
+       
+   }
+    
+    return result.length ? result : [-1]
+}
