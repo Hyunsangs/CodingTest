1dc419e648a2a8f3923ac70bea17823021a9f3b9
Hyunsangs <122857450+Hyunsangs@users.noreply.github.com>
2023-11-01 14:50:01 +0900

    [level unrated] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,21 +1,13 @@
 function solution(arr, idx) {
     var result = 0;
     
-    for (let i = 0 ; i < arr.length ; i++) {
+    for (let i = idx ; i < arr.length ; i++) {
         
-        if( arr[idx] === 1) {
-            result = idx
-            break
-            console.log(result)
-        } else if( arr[idx + i] === 1) {
-            result = idx + i 
-            break
-            console.log(result)
-        } else if ( arr.length === idx + i) {
-            return -1
-        } 
+        if ( arr[i] == 1) {
+            return i
+        }
         
     }
     
-    return result;
+    return -1
 }
