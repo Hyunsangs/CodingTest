8225d890564c3fca2af0578d353f06a4fe1d9d0b
Hyunsangs <122857450+Hyunsangs@users.noreply.github.com>
2023-11-02 12:10:38 +0900

    [level unrated] Title: 리스트 자르기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,18 @@
+function solution(n, slicer, num_list) {
+    var answer = [];
+    const [a, b ,c ] = slicer
+    
+    if ( n == 1) {
+        return num_list.slice(0,b + 1)
+    } else if ( n == 2) {
+        return num_list.slice(a)
+    } else if ( n == 3) {
+        return num_list.slice(a , b + 1)
+    } else {
+        return num_list.slice(a, b + 1).filter((_, index) => index % c === 0);
+        
+    } 
+        
+    
+    return answer;
+}
