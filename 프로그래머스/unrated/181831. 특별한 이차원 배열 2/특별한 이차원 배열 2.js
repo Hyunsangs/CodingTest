b9f93567bbdb61505546a4986856bd45deff3486
Hyunsangs <122857450+Hyunsangs@users.noreply.github.com>
2023-11-29 12:54:36 +0900

    [level unrated] Title: 특별한 이차원 배열 2, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 1; j < arr[i].length; j++) {
+            if (arr[i][j] !== arr[j][i]) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
